d4cb2a68b4953b1b5ecf5ad83ae9c2e0d0f5ab36
Garrison Snelling <garrison@hey.com>
2022-06-17 12:26:05 -0500

    Create sb-examples.sh

---
+++ sb-examples.sh
@@ -0,0 +1,42 @@
+cd examples
+
+cd ably && npm install && cd ..
+cd accessControl && npm install && cd ..
+cd antdAuditLog && npm install && cd ..
+cd authProvider && npm install && cd ..
+cd authentication && npm install && cd ..
+cd authorization && npm install && cd ..
+cd base && npm install && cd ..
+cd baseHeadless && npm install && cd ..
+cd blog && npm install && cd ..
+cd calendar && npm install && cd ..
+cd core && npm install && cd ..
+cd customPages && npm install && cd ..
+cd customization && npm install && cd ..
+cd dataProvider && npm install && cd ..
+cd e2e  && npm install && cd ..
+cd field && npm install && cd ..
+cd fineFoods && npm install && cd ..
+cd form && npm install && cd ..
+cd i18n && npm install && cd ..
+cd importExport && npm install && cd ..
+cd inputs && npm install && cd ..
+cd javascript && npm install && cd ..
+cd list/useSimpleList && npm install && cd ..
+cd multi-level-menu && npm install && cd ..
+cd multi-tenancy && npm install && cd ..
+cd mutationMode && npm install && cd ..
+cd reactToastify && npm install && cd ..
+cd real-world-example && npm install && cd ..
+cd refine-next && npm install && cd ..
+cd routerProvider/react-location && npm install && cd ..
+cd search && npm install && cd ..
+cd storybook && npm install && cd ..
+cd table && npm install && cd ..
+cd tutorial && npm install && cd ..
+cd ui/useModal && npm install && cd ..
+cd upload && npm install && cd ..
+cd vite && npm install && cd ..
+cd web3/ethereumLogin && npm install && cd ..
+
+cd ..
